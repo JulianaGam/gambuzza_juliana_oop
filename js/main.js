75446ee3b735c961bbdf89b27a8c674da37896f5
Juliana <julianagambuzza@gmail.com>
2026-03-09 15:42:38 -0400

    Update js with class files
    This commit is adding the class JS to start working on the functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-import { counter } from './modules/counter.js';
+import { Counter } from './counter.js';
 
-counter();
+const counter1 = new Counter("#counter-container1", 10);
+const counter2 = new Counter("#counter-container2", 5);
+const newCounter = new Counter("#counterCon", 0);
+const newCounter2 = new Counter("#counterCon2", 0);
+
+
+// const step10 = new StepCounter("#step10", 0, 10);
+// const step5 = new StepCounter("id", starting number, step);
--- js/modules/counter.js
+++ js/modules/counter.js
@@ -1,4 +1,114 @@
-export function counter () {   
+export class Counter {
 
+    constructor(selector, initialValue = 0){
+        this.count = initialValue;
+        this.selector = selector;
+        this.mount();
+    }
+
+    mount(){
+        const container = document.querySelector(this.selector);
+
+        this.display = document.createElement("div");
+        this.button = document.createElement("button");
+
+        // add buttons, decrement, reset
+        //this.decrement 
+        //this.reset 
+
+
+        // set button text 
+        this.button.textContent = "Increment";
+
+        //this.decrement...
+
+        //this.reset...
+
+        //Append display and button into the container 
+        container.appendChild(this.display);
+        container.appendChild(this.button);
+
+        //container.appendChild(this.decrement);
+        
+        //container.appendChild(this.reset);
+
+
+        //add Event listener 
+        this.button.addEventListener("click", () => {
+            this.increment();
+        });
+
+        //add new event listeners for decrement and reset
+
+
+        // when this first gets mounted update the display 
+        this.update();
+
+    }
+
+    //state methods
+    increment(){
+        this.count++;
+        this.update();
+    }
+
+    update(){
+        //set initial display content 
+        this.display.textContent = `count: ${this.count}`;
+        //pseudo code for decrement and reset update methods
+
+        //classList.toggle() this.count===0
+
+
+
+    }
+
+    // add decrement method
+    decrement(){
+        //if statement neeeded to prevent count from going below 0
+        if(this.count > 0){
+            this.count--;
+            this.update();
+        }
+        this.count--;
+        this.update();
+    }
+    
+    // add reset method
+
+    // reset method should set count back to 0 and update the display
+    reset(){
+
+    this.count = 0;
+    this.update();      
+}
+
+
+// counter is super class 
+//stepCounter is sub class 
 
 }
+
+class StepCounter extends Counter {
+    constructor(selector, initialValue = 0, step = 1){
+        super(selector, initialValue);
+        // add step property 
+        // this.stet=step
+        this.step = step;
+    }
+
+    //need to redefine the increment method to use the step property instead of just adding 1
+    increment(){
+        this.count += this.step;
+        this.update();
+    }
+
+     //need to redefine the decrement method to use the step property instead of just subtracting 1
+     decrement(){
+        if(this.count > 0){
+            this.count -= this.step;
+            this.update();
+        }
+    }
+}
+
