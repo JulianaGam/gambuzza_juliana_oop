39426723f4705ad6d745a5a021eb6c32a0321a0a
Juliana <julianagambuzza@gmail.com>
2026-03-17 19:49:13 -0400

    create scroll animation
    This commit is creating a scroll animation for the nav links and the sections, it creates the module and import to the main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { Counter, StepCounter } from './modules/counter.js';
+import { ScrollAnimation } from './modules/ScrollAnimation.js';
 
 const counter1 = new Counter("#counter-container1", 10);
 const counter2 = new Counter("#counter-container2", 5);
@@ -6,6 +7,4 @@ const counter3 = new StepCounter("#counter-container3", 0, 3);
 const counter4 = new StepCounter("#counter-container4", 0, 10);
 
 
-
-// const step10 = new StepCounter("#step10", 0, 10);
-// const step5 = new StepCounter("id", starting number, step);
+ScrollAnimation();
